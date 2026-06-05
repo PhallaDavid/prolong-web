9fef415e23e7f96774c6fe09a9ba0367e3a1c2d8
david <phalladavid@gmail.com>
2026-06-05 16:41:10 +0700

    setup project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Roboto } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+// import { Header } from "@/components/header";
+
+const roboto = Roboto({subsets:['latin'],variable:'--font-sans'});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,8 +29,9 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-sans", roboto.variable)}
     >
+      {/* <Header /> */}
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
